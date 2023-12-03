524f822d0d2ffddf718e63e2de7e2d84ceb871dc
Eyal Mazuz <mazuz.eyal@gmail.com>
2023-12-03 14:31:01 -0800

    added ensemble

--- train.py
+++ train.py
@@ -91,6 +91,121 @@ def train_xgb(df, split_type, test_set_number=None):
     run.summary['coverage_runtime'] = np.array(runs)
         
 
+def train_ensemble(df, split_type, images_path, hparams, test_set_number):
+    run = wandb.init(
+    # Set the project where this run will be logged
+    project="MAPF-ViT",
+    # Track hyperparameters and run metadata
+    tags=['Ensemble'])
+
+    success_order = ['sat Success', 'icts Success', 'cbsh-c Success', 'lazycbs Success', 'epea Success']
+    runtime_order = ['sat Runtime', 'icts Runtime', 'cbsh-c Runtime', 'lazycbs Runtime', 'epea Runtime']
+
+    feature_columns = ['GridColumns', 'GridRows', 'GridSize', 'NumOfObstacles', 'ObstacleDensity',
+       'AvgDistanceToGoal', 'MaxDistanceToGoal', 'MinDistanceToGoal',
+       'AvgStartDistances', 'AvgGoalDistances', 'PointsAtSPRatio', 'Sparsity',
+       '2waycollisions', 'StdDistanceToGoal', 'MaxStartDistances',
+       'MaxGoalDistances', 'MinStartDistances', 'MinGoalDistances',
+       'StdStartDistances', 'StdGoalDistances']
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    train_transforms = transforms.Compose(
+    [
+        transforms.Resize((hparams["image_size"], hparams["image_size"]), antialias=True),
+        transforms.Normalize(mean=[0, 0, 0], std=[1, 1, 1]),
+    ]
+    )
+
+    validation_transforms = transforms.Compose(
+        [
+            transforms.Resize((hparams["image_size"], hparams["image_size"]), antialias=True),
+            transforms.Normalize(mean=[0, 0, 0], std=[1, 1, 1]),
+        ]
+    )
+
+    for i, (train_df, test_df, name) in enumerate(get_split(df, split_type, test_set_number)):
+
+        # Train XGBoost model
+
+        clf = XGBClassifier(objective='multi:softmax', n_jobs=-1,
+                            callbacks=[WandbCallback(log_model=True,
+                                                     log_feature_importance=True)])
+
+        run.tags  = run.tags + (name[0], )
+
+        train_df.Y = train_df.Y.apply(lambda alg: alg2label[alg])
+        test_df.Y = test_df.Y.apply(lambda alg: alg2label[alg])
+        clf.fit(train_df[feature_columns], train_df.Y)
+
+        preds = torch.from_numpy(clf.predict_proba(test_df[feature_columns]))
+
+        # Train ViT model
+
+        train_data = MAPFDataset(images_path, train_df, transform=train_transforms)
+        val_data = MAPFDataset(images_path, test_df, transform=validation_transforms)
+
+        train_loader = DataLoader(dataset = train_data, batch_size=hparams["batch_size"], shuffle=True, num_workers=8)
+        valid_loader = DataLoader(dataset = val_data, batch_size=hparams["batch_size"], shuffle=False, num_workers=8)
+
+        run.tags  = run.tags + (name[0], images_path.split('/')[-1])
+        print(f'train data len: {len(train_data)}')
+        print(f'test data len: {len(val_data)}')
+        print(f'train loader len: {len(train_loader)}')
+        print(f'test loader len: {len(valid_loader)}')
+
+        model = SimpleViT(
+            image_size = hparams["image_size"],
+            patch_size = hparams["patch_size"],
+            num_classes = len(alg2label.keys()),
+            dim = hparams["dim"],
+            depth = hparams["depth"],
+            heads = hparams["heads"],
+            mlp_dim = hparams["mlp_dim"]
+        ).to(device)
+
+        model_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+        print(f'ViT parameters: {model_params}')
+
+        # loss function
+        criterion = nn.CrossEntropyLoss()
+        # optimizer
+        optimizer = optim.Adam(model.parameters(), lr=hparams["lr"])
+        # scheduler
+        # scheduler = StepLR(optimizer, step_size=1, gamma=gamma)
+
+        for epoch in range(hparams["epochs"]):
+            train_metrics = train_one_epoch(model, criterion, optimizer, train_loader, device)
+
+            validation_metrics = ensemble_validation_step(model, criterion, valid_loader, device)
+            
+
+            print(f"{train_metrics=}")
+            print(f"{validation_metrics=}")
+
+            run.log(train_metrics, commit=False)
+            run.log(validation_metrics, commit=True)
+
+        
+        accuracy = calc_fastest(preds, torch.tensor(test_df.Y.tolist()))
+        coverage = calc_coverage(preds, torch.from_numpy(test_df.loc[:, success_order].values.astype(np.int64)))
+        coverage_runtime = calc_coverage_runtime(preds, torch.from_numpy(test_df.loc[:, runtime_order].values.astype(np.int64)))
+        
+        accs.append(accuracy)
+        covs.append(coverage)
+        runs.append(coverage_runtime)
+
+    run.log({
+        "eval/accuracy": accuracy,
+        "eval/coverage": coverage,
+        "eval/coverage runtime": coverage_runtime,
+    })
+
+
+    run.summary['accuracy'] = np.array(accs)
+    run.summary['coverage'] = np.array(covs)
+    run.summary['coverage_runtime'] = np.array(runs)
+
 def train_vit(df, split_type, images_path, hparams, test_set_number=None):
     run = wandb.init(
     # Set the project where this run will be logged
@@ -319,6 +434,57 @@ def validation_step(model, criterion, valid_loader, device):
 
             coverage_accuracy_runtime = calc_coverage_runtime(val_output, runtimes)
             epoch_val_coverage_runtime += coverage_accuracy_runtime / len(valid_loader)
+    
+    return {
+        "eval/accuracy": epoch_val_accuracy,
+        "eval/coverage": epoch_val_coverage,
+        "eval/coverage runtime": epoch_val_coverage_runtime,
+        "eval/loss": epoch_val_loss
+    }
+
+def ensemble_validation_step(model, criterion, valid_loader, device, xgboost_preds):
+    model.eval()
+    with torch.no_grad():
+        epoch_val_accuracy = 0
+        epoch_val_coverage = 0
+        epoch_val_coverage_runtime = 0
+        epoch_val_loss = 0
+
+
+        preds = []
+        labels = []
+        successes = []
+        runtimes = []
+        for data, label, success, runtime in tqdm(valid_loader, leave=False):
+            data = data.to(device)
+            label = label.to(device)
+            success = success.to(device)
+            runtime = runtime.to(device)
+            
+            with amp.autocast():
+                val_output = model(data)
+                val_loss = criterion(val_output, labels)
+
+            epoch_val_loss += val_loss / len(valid_loader) 
+            
+            preds.append(val_output.cpu().numpy().tolist())
+            labels.append(label.cpu().numpy().tolist())
+            successes.append(success.cpu().numpy().tolist())
+            runtimes.append(runtime.cpu().numpy().tolist())
+
+        emsemble_preds = (xgboost_preds + torch.tensor(preds)) / 2
+        labels = torch.tensor(labels)
+        successes = torch.tensor(successes)
+        runtimes = torch.tensor(runtimes)
+
+        fastest_accuracy = calc_fastest(ensemble_preds, labels)
+        epoch_val_accuracy = fastest_accuracy
+
+        coverage_accuracy = calc_coverage(ensemble_preds, successes)
+        epoch_val_coverage = coverage_accuracy
+
+        coverage_accuracy_runtime = calc_coverage_runtime(ensemble_preds, runtimes)
+        epoch_val_coverage_runtime = coverage_accuracy_runtime
     
     return {
         "eval/accuracy": epoch_val_accuracy,
